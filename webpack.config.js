c9a0ff34f0f4cabb3bb244f11d9dd883b33cf59f
jingyu.zj <jingyu.zj@alibaba-inc.com>
2019-11-02 22:05:27 +0800

    feat: dev config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
+const path = require('path')
+
 module.exports = {
+  mode: 'development',
   entry: './lib/index.tsx',
+  output: {
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, './dist'),
+    library: 'cui',
+    libraryTarget: 'umd',
+  },
   module: {
     rules: [
       {
