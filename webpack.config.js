f816ae251f01d0d897852c3ab8f3dca635e6c924
jingyu.zj <jingyu.zj@alibaba-inc.com>
2019-11-03 00:23:35 +0800

    feat: add react dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './lib/index.tsx',
   output: {
     // eslint-disable-next-line no-undef
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './dist/lib'),
     library: 'cui',
     libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
   module: {
     rules: [
@@ -17,4 +21,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // eslint-disable-next-line no-undef
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+  ],
 }
