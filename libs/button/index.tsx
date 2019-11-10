97c0ae025a998287af71849be620d4d8df75ac12
jingyu <jingyu.zj@alibaba-inc.com>
2019-11-10 20:26:43 +0800

    feat: xmind add button

--- libs/button/index.tsx
+++ libs/button/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 
-function Button(props: { children?: React.ReactNode }) {
+interface Props {
+  type?: string
+  children?: React.ReactNode
+}
+
+function Button(props: Props) {
   return <div>{props.children}</div>
 }
 
