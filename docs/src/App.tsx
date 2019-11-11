e4048efc4d010af2c7218afb19db79e40f216c2a
jingyu <jingyu.zj@alibaba-inc.com>
2019-11-11 20:50:07 +0800

    feat: button component add round circle disabled prop

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -14,19 +14,29 @@ export default () => {
       </div>
       <div>
         <h1>- Button</h1>
-        <Button>Click me</Button>
-        <Button className="button-test" type="primary">
-          Click me
-        </Button>
-        <Button className="button-test" type="success">
-          Click me
-        </Button>
-        <Button className="button-test" type="warning">
-          Click me
-        </Button>
-        <Button className="button-test" type="danger">
-          Click me
-        </Button>
+        <div>
+          <Button>Click me</Button>
+          <Button round className="button-test" style={{ fontSize: '20px' }} type="primary">
+            Click me
+          </Button>
+          <Button circle className="button-test" type="success"></Button>
+          <Button disabled onClick={() => console.log(1)} className="button-test" type="warning">
+            Click me
+          </Button>
+          <Button className="button-test" type="danger">
+            Click me
+          </Button>
+        </div>
+        <div>
+          <Button disabled>Click me</Button>
+          <Button disabled type="primary">
+            Click me
+          </Button>
+        </div>
+        <div>
+          <Button>Click me</Button>
+          <Button type="primary">Click me</Button>
+        </div>
       </div>
       <Input>this is input</Input>
     </>
--- libs/button/index.tsx
+++ libs/button/index.tsx
@@ -6,12 +6,40 @@ interface Props {
   type?: string
   children?: React.ReactNode
   className?: string
+  plain?: boolean
+  round?: boolean
+  circle?: boolean
+  loading?: boolean
+  disabled?: boolean
   [key: string]: any
 }
 
-const Button: React.FC<Props> = ({ type = 'default', children = null, className, ...props }) => {
+const Button: React.FC<Props> = ({
+  type = 'default',
+  children = null,
+  className = '',
+  plain = false,
+  round = false,
+  circle = false,
+  loading = false,
+  disabled = false,
+  ...props
+  // TODO: plain, loading
+}) => {
+  const getClassName = (hasClassName: boolean | string, className: string) => {
+    return hasClassName ? className : ''
+  }
   return (
-    <button className={classnames('c-button', `c-button-${type}`, className)} {...props}>
+    <button
+      className={classnames(
+        'c-button',
+        `c-button-${type ? type : 'default'}`,
+        getClassName(round, 'is-round'),
+        getClassName(circle, 'is-circle'),
+        className,
+      )}
+      disabled={disabled}
+      {...props}>
       {children}
     </button>
   )
