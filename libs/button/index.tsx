59323f859279172e967b7dc67fab966b67689b7e
jingyu.zj <jingyu.zj@alibaba-inc.com>
2019-12-17 00:07:10 +0800

    refactor: dialog, not done

--- libs/button/index.tsx
+++ libs/button/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import classnames from 'classnames'
 import { setPrefixClassName } from '../utils'
 
-import { Icon } from '../'
+import Icon from '../icon'
 
 import './style.scss'
 
