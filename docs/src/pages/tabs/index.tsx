64ac5904b766f9b55af55f9cb57106848ba770c7
jingyu.zj <jingyu.zj@alibaba-inc.com>
2019-12-22 15:25:56 +0800

    chore: tabs component basis done

--- docs/src/pages/tabs/index.tsx
+++ docs/src/pages/tabs/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Tabs, Foldcard } from '../../../../libs'
+import { Tabs, Card, Collapse, Foldcard } from '../../../../libs'
 import Codebox from '../../components/codebox'
 import { basic } from './example'
 
@@ -13,11 +13,47 @@ export default () => {
           <h2 className="text-content">上中下布局</h2>
           <Foldcard fold={<Codebox source={basic} />} foldName="展开代码">
             <Tabs defaultActiveKey="1">
-              <TabPane tab="选项一" key="1">
-                <div>this is tab1</div>
+              <TabPane tab="选项一test" key="1">
+                <Card className="w-full">
+                  <Collapse>
+                    <Collapse.Item title="collapse1">1</Collapse.Item>
+                    <Collapse.Item title="collapse2">2</Collapse.Item>
+                    <Collapse.Item title="collapse3">3</Collapse.Item>
+                  </Collapse>
+                </Card>
               </TabPane>
               <TabPane tab="选项二" key="2">
+                <div className="w-full bg-blue">
+                  <h1>选项二内容</h1>
+                  <h1>选项二内容</h1>
+                  <h1>选项二内容</h1>
+                </div>
+              </TabPane>
+              <TabPane tab="选项三" key="3">
                 <div>this is tab3</div>
+              </TabPane>
+            </Tabs>
+          </Foldcard>
+        </div>
+        <div className="w-p-48">
+          <h2 className="text-content">上中下布局</h2>
+          <Foldcard fold={<Codebox source={basic} />} foldName="展开代码">
+            <Tabs defaultActiveKey="2" destoryOnChange={true}>
+              <TabPane tab="选项一test" key="1">
+                <Card className="w-full">
+                  <Collapse>
+                    <Collapse.Item title="collapse1">1</Collapse.Item>
+                    <Collapse.Item title="collapse2">2</Collapse.Item>
+                    <Collapse.Item title="collapse3">3</Collapse.Item>
+                  </Collapse>
+                </Card>
+              </TabPane>
+              <TabPane tab="选项二" key="2">
+                <div className="w-full bg-blue">
+                  <h1>选项二内容</h1>
+                  <h1>选项二内容</h1>
+                  <h1>选项二内容</h1>
+                </div>
               </TabPane>
               <TabPane tab="选项三" key="3">
                 <div>this is tab3</div>
--- libs/tabs/index.tsx
+++ libs/tabs/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useState, useEffect, useRef } from 'react'
 import classnames from 'classnames'
 import { setPrefixClassName } from '../utils'
 
@@ -13,16 +14,89 @@ interface TabsFC extends React.FC<Props> {
 interface Props {
   className?: string
   defaultActiveKey?: string
+  destoryOnChange?: boolean
 }
 
 const setClass = setPrefixClassName('coo-tabs')
 
 const Tabs: TabsFC = props => {
-  const { children, className, defaultActiveKey, ...leftProps } = props
-  // const length = children.length
+  const { children, className, defaultActiveKey, destoryOnChange = false, ...leftProps } = props
+
+  const [tabsWidthList, setTabsWidthList] = useState()
+
+  const [active, setActive] = useState(
+    defaultActiveKey || (children as React.ReactElement[])[0].key || '',
+  )
+
+  const tabsRef = useRef(null)
+
+  const onTabClick = (key: any) => {
+    setActive(key)
+  }
+
+  useEffect(() => {
+    const tabsElement = tabsRef.current as any
+    if (tabsElement !== null && tabsElement.children.length > 0) {
+      const widthList = Array.prototype.map
+        .call(tabsElement.children, tab =>
+          tab.className.includes('coo-tabs-tab-item') ? getComputedStyle(tab)['width'] : null,
+        )
+        .filter((d: any) => d !== null)
+      setTabsWidthList(widthList)
+    }
+  }, [tabsRef])
+
+  console.log(tabsWidthList)
+
   return (
     <div className={classnames(setClass(), className)} {...leftProps}>
-      {children}
+      <ul ref={tabsRef} className={setClass('tab')}>
+        {/* tab选项卡 */}
+        {React.Children.map(children, child => {
+          const currentChild = child as React.ReactElement
+          if (currentChild.type === TabPane) {
+            const key = currentChild.key || ''
+            const { tab } = currentChild.props
+            return (
+              <li
+                key={key}
+                className={classnames(
+                  setClass('tab-item'),
+                  active === key && setClass('tab-item-active'),
+                )}
+                onClick={() => onTabClick(key)}>
+                {tab}
+              </li>
+            )
+          }
+          return null
+        })}
+        <div className={setClass('tab-bar')}></div>
+      </ul>
+      <div className={setClass('pane')}>
+        {React.Children.map(children, tab => {
+          const currentChild = tab as React.ReactElement
+          if (currentChild.type === TabPane) {
+            const key = currentChild.key || ''
+            if (destoryOnChange) {
+              if (key === active) {
+                return <div className={setClass('pane-content')}>{tab}</div>
+              } else {
+                return null
+              }
+            } else {
+              return (
+                <div
+                  className={setClass('pane-content')}
+                  style={{ display: key === active ? 'block' : 'none' }}>
+                  {tab}
+                </div>
+              )
+            }
+          }
+          return null
+        })}
+      </div>
     </div>
   )
 }
