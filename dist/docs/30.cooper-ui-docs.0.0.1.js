a8a4c674404d3b2c5916eaead6b2883ce5414212
jingyu.zj <jingyu.zj@alibaba-inc.com>
2020-02-23 17:19:38 +0800

    fix: test tooltip transition

---
+++ dist/docs/30.cooper-ui-docs.0.0.1.js
@@ -0,0 +1,18 @@
+;(window['webpackJsonp'] = window['webpackJsonp'] || []).push([
+  [30],
+  {
+    /***/ './docs/src/pages/checkbox/index.tsx':
+      /*!*******************************************!*\
+  !*** ./docs/src/pages/checkbox/index.tsx ***!
+  \*******************************************/
+      /*! exports provided: default */
+      /***/ function(module, __webpack_exports__, __webpack_require__) {
+        'use strict'
+        eval(
+          '__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/_react@16.12.0@react/index.js");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _libs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../libs */ "./libs/index.ts");\n\n\n/* harmony default export */ __webpack_exports__["default"] = (() => {\n    return (react__WEBPACK_IMPORTED_MODULE_0__["createElement"](react__WEBPACK_IMPORTED_MODULE_0__["Fragment"], null,\n        react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("h1", null, "- checkbox"),\n        react__WEBPACK_IMPORTED_MODULE_0__["createElement"](_libs__WEBPACK_IMPORTED_MODULE_1__["Checkbox"], null)));\n});\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9kb2NzL3NyYy9wYWdlcy9jaGVja2JveC9pbmRleC50c3guanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9kb2NzL3NyYy9wYWdlcy9jaGVja2JveC9pbmRleC50c3g/NWY3MCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgKiBhcyBSZWFjdCBmcm9tICdyZWFjdCdcbmltcG9ydCB7IENoZWNrYm94IH0gZnJvbSAnLi4vLi4vLi4vLi4vbGlicydcblxuZXhwb3J0IGRlZmF1bHQgKCkgPT4ge1xuICByZXR1cm4gKFxuICAgIDw+XG4gICAgICA8aDE+LSBjaGVja2JveDwvaDE+XG4gICAgICA8Q2hlY2tib3ggLz5cbiAgICA8Lz5cbiAgKVxufVxuIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQ0E7QUFFQTtBQUNBO0FBRUE7QUFDQTtBQUdBOyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./docs/src/pages/checkbox/index.tsx\n',
+        )
+
+        /***/
+      },
+  },
+])
