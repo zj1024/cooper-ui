c0e95f913da053cf1faafbd12aa9a3d7fd720764
jingyu.zj <jingyu.zj@alibaba-inc.com>
2020-02-26 22:41:50 +0800

    chore: wow! carousel almost down, left intercal

--- docs/src/pages/carousel/index.tsx
+++ docs/src/pages/carousel/index.tsx
@@ -4,10 +4,10 @@ import Carousel from '../../../../libs/carousel'
 export default () => {
   return (
     <>
-      <Carousel defaultActive={1}>
-        <div className="h-200 flex j-center a-center bg-blue">1</div>
-        <div className="h-200 flex j-center a-center bg-yellow">2</div>
-        <div className="h-200 flex j-center a-center bg-red">3</div>
+      <Carousel defaultActive={0} dots={true} arrow="always">
+        <div className="h-200 flex j-center a-center bg-blue">0</div>
+        <div className="h-200 flex j-center a-center bg-yellow">1</div>
+        <div className="h-200 flex j-center a-center bg-red">2</div>
       </Carousel>
     </>
   )
--- libs/carousel/index.tsx
+++ libs/carousel/index.tsx
@@ -3,106 +3,188 @@ import { useState, useEffect, useRef } from 'react'
 import classnames from 'classnames'
 import { setPrefixClassName } from '../utils'
 import Icon from '../icon'
+import Transition from '../transition'
 
 import './style.scss'
 
 interface IProps {
   className?: string
   defaultActive?: number
+  dots?: boolean
+  arrow?: 'always' | 'hover' | 'never'
+  autoplay?: boolean
+  duration?: number
   [key: string]: any
 }
 
 const setClass = setPrefixClassName('coo-carousel')
 
+let nativeActive: number
 const Carousel: React.FC<IProps> = props => {
-  const { children, className, defaultActive = 0, ...leftProps } = props
+  const {
+    className,
+    defaultActive = 0,
+    dots = true,
+    arrow = 'hover',
+    autoplay = true,
+    duration = 1500,
+    ...leftProps
+  } = props
+
+  const children = React.isValidElement(props.children) ? [props.children] : props.children
   const length = (children as React.ReactElement[]).length
+
+  // state
   const [active, setActive] = useState(defaultActive)
   const [width, setWidth] = useState('')
-  const carouselRef = useRef(null)
+  const [transitionDuration, setTransitionDuration] = useState('0ms')
+  const [canIMove, setCanIMove] = useState(true)
+  const [arrowVisible, setArrowVisible] = useState(arrow === 'always')
 
+  // ref
+  const carouselRef = useRef(null)
+  const slickRef = useRef(null)
+
+  // 对动画执行时间做一个封装
+  const setMoveTransition = () => {
+    setCanIMove(false)
+    setTransitionDuration('300ms')
+  }
+
+  // arrow用到的函数，定时器也要用到，提出来
+  const handleClickArrow = (direction: string) => {
+    console.log(canIMove, active)
+    if (!canIMove) {
+      return
+    }
+    setMoveTransition()
+    if (direction === 'left') {
+      setActive(active - 1)
+      nativeActive = active - 1
+    } else {
+      setActive(active + 1)
+      nativeActive = active + 1
+    }
+  }
+
+  // 窗口变化，宽度重新设置
   useEffect(() => {
     const resize = () => {
       const carouselWidth = getComputedStyle(carouselRef.current as any)['width']
       setWidth(carouselWidth)
     }
+
     resize()
+
     window.addEventListener('resize', resize)
     return () => window.removeEventListener('resize', resize)
   }, [])
 
+  // 监听动画逻辑
   useEffect(() => {
-    console.log(width)
-  }, [width])
+    const slickDOM = slickRef.current as any
 
-  const onIndicatorChange = (index: number) => {
-    setActive(index)
-  }
-
-  const indicator = () => (
-    <div className={setClass('indicator')}>
-      {React.Children.map(children as React.ReactElement, (child, index: number) => {
-        if (child) {
-          const isActive = index === active ? setClass('indicator-item-active') : null
-          const childClassName = classnames(
-            child.props.className,
-            setClass('indicator-item'),
-            isActive,
-          )
-          return (
-            <div
-              key={index}
-              className={childClassName}
-              onClick={() => onIndicatorChange(index)}></div>
-          )
-        }
-        return
-      })}
-    </div>
-  )
-
-  const arrow = () => {
-    const handleClickArrow = (direction: string) => {
-      if (direction === 'left') {
-        if (active <= 0) {
-          setActive(length - 1)
-        } else {
-          setActive(active - 1)
-        }
-      } else {
-        if (active >= length - 1) {
-          setActive(0)
-        } else {
-          setActive(active + 1)
-        }
+    // 根据nativeActive设置active
+    const listener = () => {
+      if (nativeActive > length - 1) {
+        nativeActive = 0
+        setActive(0)
       }
+      if (nativeActive < 0) {
+        nativeActive = length - 1
+        setActive(length - 1)
+      }
+      setTransitionDuration('0ms')
+      setCanIMove(true)
+    }
+
+    listener()
+
+    slickDOM.addEventListener('transitionend', listener)
+    return () => slickDOM.removeEventListener('transitionend', listener)
+  }, [])
+
+  // 渲染指示器
+  const indicatorRender = () => {
+    const onIndicatorChange = (index: number) => {
+      setMoveTransition()
+      setActive(index)
+      nativeActive = index
     }
 
     return (
-      <>
-        <div
-          className={classnames(setClass('arrow'), setClass('arrow-left'))}
-          onClick={() => handleClickArrow('left')}>
-          <Icon name="arrow-left" />
-        </div>
-        <div
-          className={classnames(setClass('arrow'), setClass('arrow-right'))}
-          onClick={() => handleClickArrow('right')}>
-          <Icon name="arrow-right" />
-        </div>
-      </>
+      <div className={setClass('indicator')}>
+        {React.Children.map(children as React.ReactElement, (child, index: number) => {
+          if (child) {
+            const isActive = index === active ? setClass('indicator-item-active') : null
+            const childClassName = classnames(
+              child.props.className,
+              setClass('indicator-item'),
+              isActive,
+            )
+            return (
+              <div
+                key={index}
+                className={childClassName}
+                onClick={() => onIndicatorChange(index)}></div>
+            )
+          }
+          return
+        })}
+      </div>
     )
   }
 
-  const render = () => {
-    const numberWidth = parseInt(width, 10)
-    const slickStyle = {
-      width: `${numberWidth * length}px`,
-      transform: `translateX(-${numberWidth * active}px)`,
+  // 渲染箭头
+  const arrowRender = () => {
+    if (arrow === 'never') {
+      return
+    }
+
+    const arrowClassName = (direction: string) => {
+      return classnames(
+        setClass('arrow'),
+        arrow === 'hover' && setClass('arrow-hover'),
+        setClass(`arrow-${direction}`),
+      )
     }
 
     return (
-      <div className={classnames(setClass('slick'))} style={slickStyle}>
+      <Transition visible={arrowVisible}>
+        <div>
+          <div className={arrowClassName('left')} onClick={() => handleClickArrow('left')}>
+            <Icon name="arrow-left" />
+          </div>
+          <div className={arrowClassName('right')} onClick={() => handleClickArrow('right')}>
+            <Icon name="arrow-right" />
+          </div>
+        </div>
+      </Transition>
+    )
+  }
+
+  // 渲染内容
+  const render = () => {
+    const numberWidth = parseInt(width, 10)
+    const slickStyle = {
+      width: `${numberWidth * (length + 2)}px`,
+      transform: `translateX(-${numberWidth * (active + 1)}px)`,
+      transitionDuration,
+    }
+
+    const cloneElement = (cloneIndex: number) => {
+      const cloneElement = (children as React.ReactElement[])[cloneIndex]
+      const currentStyle = { ...cloneElement.props.style, width }
+      return React.cloneElement(cloneElement, {
+        key: (Math.random() * 1000).toFixed(2),
+        className: classnames(cloneElement.props.className, setClass('slick-item')),
+        style: currentStyle,
+      })
+    }
+
+    return (
+      <div ref={slickRef} className={classnames(setClass('slick'))} style={slickStyle}>
+        {cloneElement(length - 1)}
         {React.Children.map(children as React.ReactElement, (child, index: number) => {
           const currentStyle = { ...child.props.style, width }
           return React.cloneElement(child, {
@@ -111,15 +193,29 @@ const Carousel: React.FC<IProps> = props => {
             style: currentStyle,
           })
         })}
+        {cloneElement(0)}
       </div>
     )
   }
 
+  const onMouseEnter = () => {
+    arrow === 'hover' && setArrowVisible(true)
+  }
+
+  const onMouseLeave = () => {
+    arrow === 'hover' && setArrowVisible(false)
+  }
+
   return (
-    <div ref={carouselRef} className={classnames(setClass(), className)} {...leftProps}>
+    <div
+      ref={carouselRef}
+      className={classnames(setClass(), className)}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      {...leftProps}>
       {render()}
-      {indicator()}
-      {arrow()}
+      {dots && indicatorRender()}
+      {arrowRender()}
     </div>
   )
 }
