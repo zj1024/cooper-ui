c6b854a38157512579f3254dc18223c5796582f6
jingyu.zj <jingyu.zj@alibaba-inc.com>
2020-02-26 17:34:57 +0800

    chore: lower carousel

--- docs/src/pages/carousel/index.tsx
+++ docs/src/pages/carousel/index.tsx
@@ -4,7 +4,7 @@ import Carousel from '../../../../libs/carousel'
 export default () => {
   return (
     <>
-      <Carousel height="200px" defaultActive={1}>
+      <Carousel defaultActive={1}>
         <div className="h-200 flex j-center a-center bg-blue">1</div>
         <div className="h-200 flex j-center a-center bg-yellow">2</div>
         <div className="h-200 flex j-center a-center bg-red">3</div>
--- libs/carousel/index.tsx
+++ libs/carousel/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import classnames from 'classnames'
 import { setPrefixClassName } from '../utils'
+import Icon from '../icon'
 
 import './style.scss'
 
 interface IProps {
   className?: string
-  height: string
   defaultActive?: number
   [key: string]: any
 }
@@ -15,58 +15,111 @@ interface IProps {
 const setClass = setPrefixClassName('coo-carousel')
 
 const Carousel: React.FC<IProps> = props => {
-  const { children, className, style, height, defaultActive = 0, ...leftProps } = props
+  const { children, className, defaultActive = 0, ...leftProps } = props
   const length = (children as React.ReactElement[]).length
   const [active, setActive] = useState(defaultActive)
+  const [width, setWidth] = useState('')
+  const carouselRef = useRef(null)
 
   useEffect(() => {
-    console.log(length, setActive)
+    const resize = () => {
+      const carouselWidth = getComputedStyle(carouselRef.current as any)['width']
+      setWidth(carouselWidth)
+    }
+    resize()
+    window.addEventListener('resize', resize)
+    return () => window.removeEventListener('resize', resize)
   }, [])
+
+  useEffect(() => {
+    console.log(width)
+  }, [width])
 
   const onIndicatorChange = (index: number) => {
     setActive(index)
   }
 
-  const indicator = () => {
+  const indicator = () => (
+    <div className={setClass('indicator')}>
+      {React.Children.map(children as React.ReactElement, (child, index: number) => {
+        if (child) {
+          const isActive = index === active ? setClass('indicator-item-active') : null
+          const childClassName = classnames(
+            child.props.className,
+            setClass('indicator-item'),
+            isActive,
+          )
+          return (
+            <div
+              key={index}
+              className={childClassName}
+              onClick={() => onIndicatorChange(index)}></div>
+          )
+        }
+        return
+      })}
+    </div>
+  )
+
+  const arrow = () => {
+    const handleClickArrow = (direction: string) => {
+      if (direction === 'left') {
+        if (active <= 0) {
+          setActive(length - 1)
+        } else {
+          setActive(active - 1)
+        }
+      } else {
+        if (active >= length - 1) {
+          setActive(0)
+        } else {
+          setActive(active + 1)
+        }
+      }
+    }
+
     return (
-      <div className={setClass('indicator')}>
+      <>
+        <div
+          className={classnames(setClass('arrow'), setClass('arrow-left'))}
+          onClick={() => handleClickArrow('left')}>
+          <Icon name="arrow-left" />
+        </div>
+        <div
+          className={classnames(setClass('arrow'), setClass('arrow-right'))}
+          onClick={() => handleClickArrow('right')}>
+          <Icon name="arrow-right" />
+        </div>
+      </>
+    )
+  }
+
+  const render = () => {
+    const numberWidth = parseInt(width, 10)
+    const slickStyle = {
+      width: `${numberWidth * length}px`,
+      transform: `translateX(-${numberWidth * active}px)`,
+    }
+
+    return (
+      <div className={classnames(setClass('slick'))} style={slickStyle}>
         {React.Children.map(children as React.ReactElement, (child, index: number) => {
-          if (child) {
-            const isActive = index === active ? setClass('indicator-item-active') : null
-            const childClassName = classnames(
-              child.props.className,
-              setClass('indicator-item'),
-              isActive,
-            )
-            return (
-              <div
-                key={index}
-                className={childClassName}
-                onClick={() => onIndicatorChange(index)}></div>
-            )
-          }
-          return
+          const currentStyle = { ...child.props.style, width }
+          return React.cloneElement(child, {
+            key: index,
+            className: classnames(child.props.className, setClass('slick-item')),
+            style: currentStyle,
+          })
         })}
       </div>
     )
   }
 
-  const render = () => {
-    return React.Children.map(children as React.ReactElement, (child, index: number) => {
-      const isActive = index === active ? setClass('slick-active') : setClass('slick-left')
-      const childClassName = classnames(child.props.className, setClass('slick'), isActive)
-
-      return React.cloneElement(child, {
-        key: index,
-        className: childClassName,
-      })
-    })
-  }
-
   return (
-    <div className={classnames(setClass(), className)} style={{ ...style, height }} {...leftProps}>
+    <div ref={carouselRef} className={classnames(setClass(), className)} {...leftProps}>
       {render()}
       {indicator()}
+      {arrow()}
     </div>
   )
 }
