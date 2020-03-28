453df4cc787e45a44076fd03322f63dd3dcc6a60
jingyu.zj <jingyu.zj@alibaba-inc.com>
2020-03-28 23:08:33 +0800

    chore: remove hot-loader

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 import App from './App'
 
 import 'assembly-css/lib/index.scss'
@@ -8,22 +7,4 @@ import '../../cooper-theme/index.scss'
 import './style/index.scss'
 import './style.scss'
 
-ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  document.querySelector('#root'),
-)
-
-if (module.hot) {
-  module.hot.accept('./App.tsx', function() {
-    // 重新获取App，解除旧组件事件等
-    const App = require('./App').default
-    ReactDOM.render(
-      <AppContainer>
-        <App />
-      </AppContainer>,
-      document.querySelector('#root'),
-    )
-  })
-}
+ReactDOM.render(<App />, document.querySelector('#root'))
